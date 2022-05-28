0135376361cd8889183edff5807c8c061b632d71
nico <nico@aleph.engineering>
2022-05-28 12:46:51 -0400

    #3 Create AppBar.vue

---
+++ src/components/sidebar/AppBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <div class="d-flex align-center">
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink mr-2"
+        contain
+        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+        transition="scale-transition"
+        width="70"
+      />
+
+      <v-card-text class="font-weight-bold">CAOL FRONTEND</v-card-text>
+    </div>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "AppBar",
+};
+</script>
