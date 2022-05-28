ffa2c92f56f8d02cfb0930b03b2212cfa6cb8986
nico <nico@aleph.engineering>
2022-05-28 12:24:22 -0400

    #3 Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+      <v-col class="mb-4">
+        <h1>CAOL Frontend Test</h1>
+        <v-btn class="success mt-5" dark to="/">
+          <v-icon small class="mr-2">mdi-home</v-icon> Home, sweet home
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
