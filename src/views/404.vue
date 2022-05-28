fdc122124c2d76d776071a28f134a425d119e3fe
nico <nico@aleph.engineering>
2022-05-28 11:36:23 -0400

    #3 Create 404 page

---
+++ src/views/404.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col class="mb-4">
+        <h1>Oops... missing resource</h1>
+        <v-btn class="success mt-5" dark to="/">
+          <v-icon small class="mr-2">mdi-home</v-icon> Home, sweet home
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
