04b7585dacd7fe03d3a9b0b0858ffd1bbb0a8170
nico <nico@aleph.engineering>
2022-05-28 12:17:55 -0400

    #3 Create navigation drawer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,34 @@
 <template>
   <v-app>
+    <v-navigation-drawer
+      dark
+      absolute
+      permanent
+      expand-on-hover
+      color="primary"
+    >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6"> MENU ITEMS </v-list-item-title>
+          <v-list-item-subtitle> subtext </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in links" :key="item.title" :to="item.to" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <v-img
@@ -8,15 +37,12 @@
           contain
           src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
           transition="scale-transition"
-          width="30"
+          width="70"
         />
 
         <v-card-text class="font-weight-bold">CAOL FRONTEND</v-card-text>
       </div>
-
-      <v-spacer></v-spacer>
     </v-app-bar>
-
     <v-main>
       <router-view />
     </v-main>
@@ -28,7 +54,11 @@ export default {
   name: "App",
 
   data: () => ({
-    //
+    links: [
+      { to: "/", title: "Home", icon: "mdi-home" },
+      { to: "/consultants", title: "Consultants", icon: "mdi-account-group" },
+      { to: "/about", title: "About", icon: "mdi-information" },
+    ],
   }),
 };
 </script>
