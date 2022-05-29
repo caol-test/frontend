9e6eed7d66e42ea07158e6f10a989a9b77301f90
nico <nico@aleph.engineering>
2022-05-29 14:33:23 -0400

    #4 Adjust users table component

--- src/components/consultants/children/UsersTable.vue
+++ src/components/consultants/children/UsersTable.vue
@@ -1,11 +1,13 @@
 <template>
   <v-data-table
     v-model="selected"
-    :headers="headers"
-    :items="desserts"
-    item-key="name"
+    item-key="co_usuario"
     show-select
     class="elevation-1"
+    loading-text="Loading... please wait"
+    :loading="loadingConsultants"
+    :headers="headers"
+    :items="consultants"
   >
   </v-data-table>
 </template>
@@ -14,103 +16,46 @@
 export default {
   data() {
     return {
+      loadingConsultants: true,
       selected: [],
       headers: [
-        {
-          text: "Dessert (100g serving)",
-          align: "start",
-          sortable: false,
-          value: "name",
-        },
-        { text: "Calories", value: "calories" },
-        { text: "Fat (g)", value: "fat" },
-        { text: "Carbs (g)", value: "carbs" },
-        { text: "Protein (g)", value: "protein" },
-        { text: "Iron (%)", value: "iron" },
+        { text: "Full name", value: "no_usuario", sortable: false },
+        { text: "Address", value: "ds_endereco" },
+        { text: "Email", value: "no_email" },
+        { text: "Phone number", value: "nu_telefone" },
       ],
-      desserts: [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: "1%",
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: "1%",
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: "7%",
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: "8%",
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: "16%",
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: "0%",
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: "2%",
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: "45%",
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: "22%",
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: "6%",
-        },
-      ],
+      consultants: [],
     };
+  },
+  methods: {
+    async getJwt() {
+      let response = await this.$axios({ url: "jwt" });
+      let responseData = this.getResponseData(response);
+
+      this.$store.commit("setJwt", responseData.jwt);
+    },
+    async loadConsultants() {
+      await this.getJwt();
+
+      this.$axios({ url: "users/consultants" })
+        .then((response) => {
+          this.loadingConsultants = false;
+          this.consultants = this.getResponseData(response);
+        })
+        .catch((error) => {
+          this.loadingConsultants = false;
+          this.consultants = [];
+
+          console.log(error);
+        });
+    },
+    getResponseData(response) {
+      let { data: responseData } = response;
+      return responseData.data;
+    },
+  },
+  mounted() {
+    this.loadConsultants();
   },
 };
 </script>
