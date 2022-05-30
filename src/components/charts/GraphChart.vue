76eb158481f251f6dd0fb7fd38eee71a9617fbc1
nico <nico@aleph.engineering>
2022-05-30 00:24:34 -0400

    #6 Refactor GraphChart.vue

--- src/components/charts/GraphChart.vue
+++ src/components/charts/GraphChart.vue
@@ -1,130 +1,58 @@
 <template>
   <div id="chart">
-    <apexchart type="line" height="350" :options="chartOptions" :series="chartSeries"></apexchart>
+    <apex-chart
+      type="line"
+      height="350"
+      :options="chartOptions"
+      :series="chartSeries"
+    />
   </div>
 </template>
 
 <script>
-import VueApexCharts from 'vue-apexcharts'
+import VueApexCharts from "vue-apexcharts";
 
 export default {
-  name: 'Chart',
+  name: "Chart",
 
   data: () => ({
-    chartSeries: [
-      {
-        name: 'Income',
-        type: 'column',
-        data: [1.4, 2, 2.5, 1.5, 2.5, 2.8, 3.8, 4.6]
-      },
-      {
-        name: 'Cashflow',
-        type: 'column',
-        data: [1.1, 3, 3.1, 4, 4.1, 4.9, 6.5, 8.5]
-      },
-      {
-        name: 'Revenue',
-        type: 'line',
-        data: [20, 29, 37, 36, 44, 45, 50, 58]
-      }
-    ],
     chartOptions: {
       stroke: {
-        width: [1, 1, 4]
+        width: [1, 1, 4],
       },
       title: {
-        text: 'XYZ - Stock Analysis (2009 - 2016)',
-        align: 'left',
-        offsetX: 110
+        text: "Consultant Performance",
+        align: "left",
+        offsetX: 110,
       },
       xaxis: {
-        categories: [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016],
+        categories: [],
       },
-      yaxis: [
-        {
-          axisTicks: {
-            show: true,
-          },
-          axisBorder: {
-            show: true,
-            color: '#008FFB'
-          },
-          labels: {
-            style: {
-              colors: '#008FFB',
-            }
-          },
-          title: {
-            text: "Income (thousand crores)",
-            style: {
-              color: '#008FFB',
-            }
-          },
-          tooltip: {
-            enabled: true
-          }
-        },
-        {
-          seriesName: 'Income',
-          opposite: true,
-          axisTicks: {
-            show: true,
-          },
-          axisBorder: {
-            show: true,
-            color: '#00E396'
-          },
-          labels: {
-            style: {
-              colors: '#00E396',
-            }
-          },
-          title: {
-            text: "Operating Cashflow (thousand crores)",
-            style: {
-              color: '#00E396',
-            }
-          },
-        },
-        {
-          seriesName: 'Revenue',
-          opposite: true,
-          axisTicks: {
-            show: true,
-          },
-          axisBorder: {
-            show: true,
-            color: '#FEB019'
-          },
-          labels: {
-            style: {
-              colors: '#FEB019',
-            },
-          },
-          title: {
-            text: "Revenue (thousand crores)",
-            style: {
-              color: '#FEB019',
-            }
-          }
-        },
-      ],
       tooltip: {
         fixed: {
           enabled: true,
-          position: 'topLeft', // topRight, topLeft, bottomRight, bottomLeft
+          position: "topLeft",
           offsetY: 30,
-          offsetX: 60
+          offsetX: 60,
         },
       },
       legend: {
-        horizontalAlign: 'left',
-        offsetX: 40
-      }
-    }
+        horizontalAlign: "left",
+        offsetX: 40,
+      },
+    },
   }),
+  props: ["chartSeries"],
   components: {
-    apexchart: VueApexCharts,
+    ApexChart: VueApexCharts,
   },
-}
+  methods: {
+    setChartOptions(xAxis) {
+      let newOptions = JSON.parse(JSON.stringify(this.chartOptions));
+      newOptions.xaxis.categories = xAxis;
+
+      this.chartOptions = newOptions;
+    },
+  },
+};
 </script>
