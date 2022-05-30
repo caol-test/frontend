b93638a75cd897a94ca3f806f874be4b64ee2514
nico <nico@aleph.engineering>
2022-05-30 01:17:52 -0400

    #6 Refactor Home.vue

--- src/components/consultants/Home.vue
+++ src/components/consultants/Home.vue
@@ -47,6 +47,7 @@
           color="primary"
           class="mt-5"
           :disabled="flagDisableVisualizeBtn"
+          @click="showPie()"
         >
           <v-icon class="mr-2">mdi-chart-pie</v-icon>
           View Pie
@@ -62,7 +63,11 @@
           :chart-series="chartSeries"
         />
 
-        <div v-show="graphType === 'pie'" />
+        <graph-pie
+          ref="graphPie"
+          v-show="graphType === 'pie'"
+          :chart-series="pieSeries"
+        />
 
         <graph-table v-show="graphType === 'table'" :items="earnings" />
       </v-col>
@@ -74,6 +79,7 @@
 import UsersTable from "@/components/consultants/children/UsersTable";
 import GraphChart from "@/components/charts/GraphChart";
 import GraphTable from "@/components/charts/GraphTable";
+import GraphPie from "@/components/charts/GraphPie";
 import SettingsNavDrawer from "@/components/consultants/children/SettingsNavDrawer";
 
 export default {
@@ -90,8 +96,16 @@ export default {
     earnings: [],
     chartSeries: [],
     chartXAxis: [],
+    pieSeries: [],
+    pieLabels: [],
   }),
-  components: { UsersTable, GraphChart, GraphTable, SettingsNavDrawer },
+  components: {
+    UsersTable,
+    GraphChart,
+    GraphPie,
+    GraphTable,
+    SettingsNavDrawer,
+  },
   methods: {
     setMonth(dateType, value) {
       if (dateType === "start") {
@@ -112,6 +126,13 @@ export default {
       await this.fetchMonthlyCosts();
 
       this.graphType = "chart";
+      this.dialog = true;
+    },
+    async showPie() {
+      this.dialog = false;
+      await this.fetchPercents();
+
+      this.graphType = "pie";
       this.dialog = true;
     },
     async fetchEarnings() {
@@ -147,6 +168,25 @@ export default {
       this.chartXAxis = responseData.x_axis;
 
       this.$refs.graphChart.setChartOptions(this.chartXAxis);
+    },
+    async fetchPercents() {
+      let selectedUsersIds = this.getSelectedConsultantsIds();
+
+      let response = await this.$axios({
+        url: "earnings/percent",
+        params: {
+          consultants: selectedUsersIds,
+          from: this.startMonth,
+          to: this.endMonth,
+        },
+      });
+
+      let responseData = this.$axios.getResponseData(response);
+
+      this.pieSeries = responseData.series;
+      this.pieLabels = responseData.labels;
+
+      this.$refs.graphPie.setChartOptions(this.pieLabels);
     },
     getSelectedConsultantsIds() {
       return this.selectedUsers.map((user) => user.co_usuario);
